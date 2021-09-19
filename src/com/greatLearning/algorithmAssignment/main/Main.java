e0e63b43c4d044b992e57c7f0269bd323ee0b39d
Ambika <ambikasawal91@gmail.com>
2021-09-19 17:24:54 +0530

    started assignment

--- src/com/greatLearning/algorithmAssignment/main/Main.java
+++ src/com/greatLearning/algorithmAssignment/main/Main.java
@@ -10,9 +10,8 @@ public class Main {
 	double[] stock_price_arr;
 	String[] company_name_arr;
 	boolean[] rise_in_stock_price;
-	SearchingSortingHelper searchingSortingHelper=new SearchingSortingHelper();
+	SearchingSortingHelper searchingSortingHelper = new SearchingSortingHelper();
 	Scanner sc_input = new Scanner(System.in);
-	
 
 	private void insertValues(double arr[], int size) {
 		// TODO Auto-generated method stub
@@ -30,10 +29,8 @@ public class Main {
 		System.out.println();
 	}
 
-
-
 	private void TaskImplementation() {
-		
+
 		System.out.println("Please enter total number of elements required inside array");
 
 		int arr_size = sc_input.nextInt();
@@ -41,26 +38,25 @@ public class Main {
 		stock_price_arr = new double[arr_size];
 
 		System.out.println("Please enter elements for  array");
-		insertValues(stock_price_arr,arr_size);
-		printValues(stock_price_arr,arr_size);
-		//sortArray(array, 0, array.length - 1);
+		insertValues(stock_price_arr, arr_size);
+		printValues(stock_price_arr, arr_size);
+		// sortArray(array, 0, array.length - 1);
 
-		stock_price_arr=searchingSortingHelper.sortInAscending(stock_price_arr, 0,arr_size-1);
-		printValues(stock_price_arr,arr_size);
-		
-		//binarySearch(0, arr.length - 1, search_value);
-
-		int index=searchingSortingHelper.searchElementUsingBinarySearch(stock_price_arr,0, arr_size-1,0,43);
-	
-	System.out.println(index);
+//		stock_price_arr=searchingSortingHelper.sortInAscending(stock_price_arr, 0,arr_size-1);
+//		printValues(stock_price_arr,arr_size);
+//		
+//		//binarySearch(0, arr.length - 1, search_value);
+//
+//		int index=searchingSortingHelper.searchElementUsingBinarySearch(stock_price_arr,0, arr_size-1,0,43);
+//	
+//	System.out.println(index);
 	}
 
 	public static void main(String args[]) {
 
-		Main main=new Main();
+		Main main = new Main();
 		main.TaskImplementation();
-		
-		
+
 		// CompanyDetails companydetails = new CompanyDetails();
 
 	}
